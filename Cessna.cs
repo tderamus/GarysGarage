54919b8994bf51fb94cad9905abf87c002ee15a7
tderamus <deramust@gmail.com>
2025-01-12 14:13:08 -0600

    added files for program classes

---
+++ Cessna.cs
@@ -0,0 +1,12 @@
+namespace Garage;
+
+    public class Cessna
+{
+    public double FuelCapacity { get; set; }
+    public string MainColor { get; set; }
+    public int MaximumOccupancy { get; set; }
+    public void RefuelTank()
+    {
+        // method definition omitted
+    }
+}
---
+++ Zero.cs
@@ -0,0 +1,12 @@
+namespace Garage;
+
+public class Zero
+{
+    public double BatteryKWh { get; set; }
+    public string MainColor { get; set; }
+    public int MaximumOccupancy { get; set; }
+    public void ChargeBattery()
+    {
+        // method definition omitted
+    }
+}
